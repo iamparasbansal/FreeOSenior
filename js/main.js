1e2907590bd6c1ea722dde6bfa5d216f00cdc324
Chetan <61468014+Unknown39825@users.noreply.github.com>
2020-11-26 01:08:21 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function hello()
                 console.log('Name: ' + profile.getName());
                 console.log('Image URL: ' + profile.getImageUrl());
                 console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-                getimg();
+             
             }
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
