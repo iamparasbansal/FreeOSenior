17c418fcb73fa96e253d8bafb48931dc027a6434
Chetan <61468014+Unknown39825@users.noreply.github.com>
2020-11-26 01:05:26 +0530

    authentication fixed

--- js/main.js
+++ js/main.js
@@ -70,3 +70,17 @@ $(document).ready(function () {
   AOS.init();
   
 });
+$(function() {
+    if (auth2.isSignedIn.get()) {
+  var profile = auth2.currentUser.get().getBasicProfile();
+  console.log('ID: ' + profile.getId());
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail());
+  document.getElementById("after-login").style.display="flex";
+  document.getElementById("before-login").style.display="none";
+
+}
+});
